954f44643c7feca6b0567f85ccd0ab8450d785a0
Rob Goelz <RobGoelz@gmail.com>
2019-04-08 21:17:31 +0000

    feat: create console script

--- setup.py
+++ setup.py
@@ -6,11 +6,14 @@ with open('README.rst', 'r') as f:
 setup(
         name='hr',
         version='0.1.0',
-        description='Commandline user management utility',
+        description='Command-line user management utility',
         long_description=readme,
         author='Rob Goelz',
         author_email='RobGoelz@gmail.com',
         packages=find_packages('src'),
         package_dir={'': 'src'},
-        install_requires=[]
+        install_requires=[],
+        entry_points={
+            'console_scripts': 'hr=hr.cli:main',
+        },
 )
